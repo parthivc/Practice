b79f8a4266e0f77951a9447adb8f267c1240dc27
Parthiv Chigurupati <parthiv.chigurupati@gmail.com>
2020-09-18 16:31:45 -0400

    Finished william lin tree excersize

--- FAANG/googleTree.py
+++ FAANG/googleTree.py
@@ -54,14 +54,43 @@ def subTreeNodeDepthSum(root):
 
 # Task 3: Given a target node, return the sum of node depths for all nodes to the target node
 
+def nodeSumToTarget(root, target):
+    nodeSumToTarget.answer = 0
+    def dfsHelper(root):
+        result = [1, 0]  # Number of nodes, sum of depths
+        if root.left:
+            lResult = dfsHelper(root.left)
+            result[0] += lResult[0]
+            result[1] += lResult[0] + lResult[1]
+        if root.right:
+            rResult = dfsHelper(root.right)
+            result[0] += rResult[0]
+            result[1] += rResult[0] + rResult[1]
+        root.size = result[0]
+        return result
+    nodesAndDepth = dfsHelper(root)
+    treeSize = nodesAndDepth[0]
+    def dfsHelper2(root, target, distanceSum):
+        if root.value == target:
+            nodeSumToTarget.answer = distanceSum
+        if root.left:
+            newDistanceSum = distanceSum - (root.left.size) + (treeSize - root.left.size)
+            dfsHelper2(root.left, target, newDistanceSum)
+        if root.right:
+            newDistanceSum = distanceSum - (root.right.size) + (treeSize - root.right.size)
+            dfsHelper2(root.right, target, newDistanceSum)
+    dfsHelper2(root, target, nodesAndDepth[1])
+    return nodeSumToTarget.answer
 
-
+# Test methods
 
 def main():
     # This tree represents the above tree for reference and testing
     example = Node(1, Node(2, Node(4, Node(8), Node(9)), Node(5)), Node(3, Node(6), Node(7)))
     print("\nSum of node depths: {}\n".format(nodeDepthSum(example)))
     print("Sum of all subtree node depths: {}\n".format(subTreeNodeDepthSum(example)))
+    target = 4
+    print("Sum of all subtree node depths to {}: {}\n".format(target, nodeSumToTarget(example, target)))
 
 if __name__ == "__main__":
     main()
