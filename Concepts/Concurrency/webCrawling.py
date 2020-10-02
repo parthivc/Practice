4a8d0bcfd522365028f29c7ce55986fc64079a73
Parthiv Chigurupati <parthiv.chigurupati@gmail.com>
2020-10-02 13:33:30 -0400

    finished image downloader

--- Concepts/Concurrency/webCrawling.py
+++ Concepts/Concurrency/webCrawling.py
@@ -1,6 +1,6 @@
 # This is HtmlParser's API interface.
 # You should not implement it, or speculate about its implementation
-# 
+#  https://code.dennyzhang.com/web-crawler-multithreaded
 
 class HtmlParser(object):
     def __init__(self):
@@ -26,16 +26,19 @@ class webCrawler():
         hostName = self.getHostname(baseURL)
         while queue:
             queue2 = []
+            # Temporary set
             with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
                 threadResults = list(executor.map(HtmlParser.getUrls, queue))
                 for urlLists in threadResults:
                     for url in urlLists:
                         if url not in visited and self.getHostname(url) == hostName:
                             visited.add(url)
+                            # add to temporary set
                             queue2.append(url)
             queue = queue2
+            # join temporary sets
+            # pass temporary set to a consumer worker that actually downloads everything
         return list(visited)
-
 
 
 def main():
