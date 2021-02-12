bcea85dc7f4ff03848a2aa45933bb0aabf4c1361
Parthiv Chigurupati <parthiv.chigurupati@gmail.com>
2021-02-12 16:21:49 -0500

    corrected quickselect

--- Concepts/QuickSelect.py
+++ Concepts/QuickSelect.py
@@ -4,29 +4,24 @@
 # This method finds the kth smallest element
 # To find the kth largest element, flip all inequality signs except for the while loops
 
-def quickSelect(array, k):
-    if k == 0 or k == len(array) - 1:
-        return "not possible"
+def quickSelect(nums, k):
     start = 0
-    end = len(array) - 1
-
-    while start < end:
-        midpoint = array[(start + end) // 2]
-        left = start
-        right = end
-        while left < right:
-            if array[left] >= midpoint:
-                array[right], array[left] = array[left], array[right]
-                right -= 1
-            else:
-                left += 1
-        if array[left] > midpoint:
-            left -= 1
-        if k <= left:
-            end = left
+    end = len(nums) - 1
+    while start <= end:
+        pivot = start
+        for runner in range(start, end):
+            if nums[runner] >= nums[end]:
+                nums[runner], nums[pivot] = nums[pivot], nums[runner]
+                pivot += 1
+        nums[end], nums[pivot] = nums[pivot], nums[end]
+        count = end - pivot + 1
+        if count < k:
+            k -= count
+            end = pivot - 1
+        elif count > k:
+            start = pivot + 1
         else:
-            start = left + 1
-    return array[k - 1]
+            return nums[pivot]
 
 
 # def recursiveQuickSelect(array, k)
@@ -35,6 +30,10 @@ def quickSelect(array, k):
 def main():
     array = [10, 4, 5, 8, 6, 11, 26]
     k = 3  # K is 1-indexed
+    print("\n{}\nThe kth smallest element with k = {} is".format(array, k), end=" ")
+    print("{}\n".format(quickSelect(array, k)))
+    array = [-3, -2, -3, -1, -2, -4, -5, -5, -6]
+    k = 4  # K is 1-indexed
     print("\n{}\nThe kth smallest element with k = {} is".format(array, k), end=" ")
     print("{}\n".format(quickSelect(array, k)))
 
