fb42a12363ce4a537acc966ae6c1c2226f8b0f74
Parthiv Chigurupati <parthiv.chigurupati@gmail.com>
2021-06-06 01:10:05 -0400

    updated quickselect

--- Concepts/QuickSelect.py
+++ Concepts/QuickSelect.py
@@ -4,6 +4,38 @@
 # This method finds the kth smallest element
 # To find the kth largest element, flip all inequality signs except for the while loops
 
+# import random
+# def partition(a, l, r, pivotIndex):
+#         pivotVal = a[pivotIndex]
+#         a[pivotIndex], a[r] = a[r], a[pivotIndex]
+#         storeIndex = l
+#         for i in range(l, r):
+#                 if(a[i] > pivotVal):
+#                         a[storeIndex], a[i] = a[i], a[storeIndex]
+#                         storeIndex+=1
+#         a[r], a[storeIndex] = a[storeIndex], a[r]
+#         return storeIndex
+
+# def select(a, l, r, k):
+#         if(l == r):
+#                 return l
+#         pivotIndex = random.randint(l, r)
+#         pivotIndex = partition(a, l, r, pivotIndex)
+#         if(k == pivotIndex):
+#                 return k
+#         elif(k < pivotIndex):
+#                 return select(a, l, pivotIndex-1, k)
+#         else:
+#                 return select(a, pivotIndex+1, r, k)
+
+
+# arr = [12, 40, 20, 10, 45, 13, 53]
+# n = len(arr)
+# k = 4
+# res = select(arr, 0, n-1, k-1)
+
+
+
 def quickSelect(nums, k):
     start = 0
     end = len(nums) - 1
@@ -22,10 +54,6 @@ def quickSelect(nums, k):
             start = pivot + 1
         else:
             return nums[pivot]
-
-
-# def recursiveQuickSelect(array, k)
-
 
 def main():
     array = [10, 4, 5, 8, 6, 11, 26]
